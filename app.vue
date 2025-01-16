851f9566c8308bb32f151903e6e30fbe9b003d26
Timothy Meehan <timothy.meehan@backmarket.com>
2025-01-16 14:28:38 +0100

    removed others

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
   <div>
     <div>font vs fontaine fallback vs native fallback</div>
     <h1 class="heading-1">This is a title</h1>
-    <div
+    <!-- <div
       style="
         display: flex;
         flex-direction: row;
@@ -25,6 +25,32 @@
     >
       <h1 class="heading-1-default">This is a title</h1>
       <span> (default)</span>
+    </div> -->
+
+    <h2 class="heading-2">This is a subtitle</h2>
+    <!-- <div
+      style="
+        display: flex;
+        flex-direction: row;
+        gap: 8px;
+        align-items: center;
+        justify-items: center;
+      "
+    >
+      <h2 class="heading-2-fallback">This is a subtitle</h2>
+      <span>(fallback)</span>
+    </div>
+    <div
+      style="
+        display: flex;
+        flex-direction: row;
+        gap: 8px;
+        align-items: center;
+        justify-items: center;
+      "
+    >
+      <h2 class="heading-2-default">This is a subtitle</h2>
+      <span> (default)</span>
     </div>
 
     <div>blue=custom font. red=fallback</div>
@@ -35,10 +61,8 @@
       <div style="color: red; font-family: 'BMDupletDSP fallback'">
         ABCDEFGHIJKLMNOPQRSTYUWXYZ
       </div>
-    </div>
+    </div>-->
   </div>
-
-  <div ref="child" />
 </template>
 
 <style>
@@ -58,15 +82,29 @@ html {
 
 .heading-1 {
   font-family: BMDupletTXT;
-  font-size: 140px;
+  font-size: 200px;
 }
 
 .heading-1-fallback {
   font-family: "BMDupletTXT fallback";
-  font-size: 140px;
+  font-size: 200px;
 }
 
 .heading-1-default {
-  font-size: 140px;
+  font-size: 200px;
+}
+
+.heading-2 {
+  font-family: BMDupletDSP;
+  font-size: 120px;
+}
+
+.heading-2-fallback {
+  font-family: "BMDupletDSP fallback";
+  font-size: 120px;
+}
+
+.heading-2-default {
+  font-size: 120px;
 }
 </style>
